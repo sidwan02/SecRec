405baedbd893848ee8cfcf8b4532a0ed39273469
sidwan02 <sidwan02@gmail.com>
2023-11-14 12:12:30 -0500

    added handle rating for combiner

--- src/combiner.py
+++ src/combiner.py
@@ -23,8 +23,6 @@ class Combiner:
         self.setup_server_storage({}, "movie-ownership")
         # {user: set(filled entries: tuple(int, int))}
         self.setup_server_storage({}, "owned-entries")
-        # N x M matrix with N users and M movies
-        self.setup_server_storage({}, "ratings")
         
     def setup_server_storage(self, data, description):
         palintext = util.ObjectToBytes(data)
@@ -40,10 +38,46 @@ class Combiner:
         
         self.server.storage[description] = ciphertext
         
-
-    def handle_rating(self, movie: str, rating: float):
+        self.keyDB.keys[f"{description}-encryptk"] = data_encryptk
+        self.keyDB.keys[f"{description}-decryptk"] = data_decryptk
+        self.keyDB.keys[f"{description}-signk"] = data_signk
+        self.keyDB.keys[f"{description}-verifyk"] = data_verifyk
         
-        Combiner.handle_rating(movie, rating, self.username)
+    def server_store(self, data, description):
+        palintext = util.ObjectToBytes(data)
+        
+        # sign the plaintext
+        data_sign = crypto.SignatureSign(self.keyDB.keys[f"{description}-signk"], palintext)
+        
+        # concat the sign to the plaintext, then encrypt
+        ciphertext = crypto.AsymmetricEncrypt(self.keyDB.keys[f"{description}-encryptk"], data + data_sign)
+        
+        self.server.storage[description] = ciphertext
+        
+    def retrieve_server_storage(self, description: str):
+        concat_plaintext = crypto.AsymmetricDecrypt(self.keyDB.keys[f"{description}-decryptk"], self.server.storage[description])
+        plaintext, signature = concat_plaintext[:-512], concat_plaintext[-512:]
+        
+        if not crypto.SignatureVerify(self.keyDB.keys[f"{description}-verifyk"], plaintext, signature):
+            pass
+        
+        return util.BytesToObject(plaintext)
+
+    def handle_rating(self, movie: str, rating: float, user: str):
+        movie_ownership_map = self.retrieve_server_storage("movie-ownership")
+        user_ownership_map = self.retrieve_server_storage("user-ownership")
+        
+        if movie not in movie_ownership_map:
+            self.server.add_movie()
+            movie_ownership_map[movie] = len(movie_ownership_map)
+            self.server_store(movie_ownership_map, "movie-ownership")
+            
+        if user not in user_ownership_map:
+            self.server.add_user()
+            user_ownership_map[user] = len(user_ownership_map)
+            self.server_store(user_ownership_map, "user-ownership")
+            
+        self.server.add_rating(rating, user_ownership_map[user], movie_ownership_map[movie])
 
     def receive_rating(self, movie: str, username: str) -> Union[None, float]:
         pass
--- src/server.py
+++ src/server.py
@@ -1,3 +1,31 @@
 class Server:
     def __init__(self):
         self.storage = {}
+        
+        # N x M matrix with N users and M movies
+        # encryption and signature of this is maintained separately by the server
+        self.ratings = [[]]
+        
+    # adds a new col
+    def add_movie(self):
+        # TODO: looking at the matrix completion assignment from 1952q, figure out what the default value should be
+        for user_ratings in self.ratings:
+            user_ratings.append(None)
+    
+    # adds a new row
+    def add_user(self):
+        num_movies = len(self.ratings[0])
+        
+        # TODO: looking at the matrix completion assignment from 1952q, figure out what the default value should be
+        self.ratings.append([None for _ in range(num_movies)])
+        
+    def add_rating(self, rating: float, r: int, c: int):
+        pass
+    
+    def matrix_completion(self):
+        pass
+    
+    def generate_statistic(self):
+        self.matrix_completion()
+        
+        pass
